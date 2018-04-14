0afddde00cc8050693752fa937c00711b185384e
zoemiyogacode <zoemiyoga@gmail.com>
2018-04-14 21:05:04 +0100

    Backup_5

--- js/app.js
+++ js/app.js
@@ -101,6 +101,7 @@ function matched() {
 	 openedCards[1].classList.add('match', 'locked');
 	 openedCards[0].classList.remove('show', 'open');
 	 openedCards[1].classList.remove('show', 'open');
+	 matchedCards.push(openedCards[0], openedCards[1]);
 	 openedCards = [];
 }
 
@@ -109,9 +110,33 @@ function matched() {
  */
 
  function unmatched() {
+ 	lock();
  	setTimeout(function() {
  		openedCards[0].classList.remove('show', 'open');
  		openedCards[1].classList.remove('show', 'open');
  		openedCards = [];
+ 		unlock();
 	},600);
+}
+
+/*
+ * @description Locking cards from responding to clicks
+ */
+
+ function lock() {
+ 	Array.prototype.filter.call(cards, function(card) {
+ 		card.classList.add('locked');
+    });
  }
+
+/*
+ * @description Unlocking not matched cards
+ */
+function unlock() {
+	Array.prototype.filter.call(cards, function(card) {
+		card.classList.remove('locked');
+		for(var i = 0; i < matchedCards.length; i++){
+			matchedCards[i].classList.add("locked");
+		}
+	});
+}
