e3f74950dc18d549f22f65dc0b9a01047a2e31bf
zoemiyogacode <zoemiyoga@gmail.com>
2018-04-14 19:05:34 +0100

    Backup_3

--- js/app.js
+++ js/app.js
@@ -8,7 +8,8 @@
  let shuffledCards = [];
  let openedCards = [];
  let moves = 0;
- let movesTracker = document.querySelector(".moves")
+ let movesTracker = document.querySelector(".moves");
+ let matchedCards = [];
 
  /*
  * @description Start the game function
@@ -46,18 +47,6 @@ function shuffle(array) {
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
  /*
  * Event listener Loop
  * @description Listens for clicks on the cards
@@ -86,7 +75,7 @@ function openCard() {
 	openedCards.push(this);
 	if(openedCards.length === 2) {
 		moveCounter();
-		if(openedCards[0] === openedCards[1]) {
+		if(openedCards[0].innerHTML === openedCards[1].innerHTML) {
 			matched();
 		} else {
 			unmatched();
@@ -104,5 +93,12 @@ function openCard() {
  }
 
 /*
- * @description Move the counter func
+ * @description Match the cards
  */
+
+function matched() {
+	 openedCards[0].classList.toggle('match');
+	 openedCards[1].classList.toggle('match');
+	 openedCards[0].classList.toggle('locked');
+	 openedCards[1].classList.toggle('match');
+}
