756fad740504134df0b171178f598e1f7d0f6570
zoemiyogacode <zoemiyoga@gmail.com>
2018-04-14 17:49:36 +0100

    Backup_1

--- js/app.js
+++ js/app.js
@@ -1,16 +1,36 @@
 /*
- * Create a list that holds all of your cards
+ * Variables
  */
 
+ let card = document.getElementsByClassName("card");
+ let cards = [...card];
+ let deck = document.querySelector(".deck");
+ let shuffledCards = [];
+ let openedCards = [];
+ let moves = 0;
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+ /*
+ * @description Start the game function
+ * @returns {array} Displays the cards randomly on the page
  */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
+ function startGame() { 
+ 	shuffledCards = shuffle(cards);
+ 	 for (let i = 0; i < cards.length; i++) {
+ 	 	deck.innerHTML = "";
+ 	 	shuffledCards.forEach(function(card) {
+ 	 		deck.appendChild(card);
+ 	 	})};
+}
+
+ /*
+ * @description Start the game
+ */
+startGame();
+
+ /*
+ * @description Predefined Shuffle function from http://stackoverflow.com/a/2450976
+ */
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -36,3 +56,46 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+ /*
+ * Event listener Loop
+ * @description Listens for clicks on the cards
+ */
+
+for (let i = 0; i < cards.length; i++) {
+	card = cards[i];
+	card.addEventListener('click', displayCard, openCard);
+}
+
+/*
+ * @description Display the card
+ */
+
+function displayCard() {
+	this.classList.toggle('open');
+	this.classList.toggle('show');
+}
+
+ /*
+ * @description Add card to opened cards, move the counter and check if the cards match
+ */
+
+function openCard() {
+	openedCards.push(this);
+	if(openedCards.length === 2) {
+		moveCounter();
+		if(openedCards[0] === openedCards[1]) {
+			matched();
+		} else {
+			unmatched();
+		}
+	}
+};
+
+ /*
+ * @description Move the counter
+ */
+
+ function moveCounter() {
+ 	moves++
+ }
